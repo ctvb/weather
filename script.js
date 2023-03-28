d68e85f6aab4b2cc05f2a2e893fe72d981c72682
ctvb <deenicks@hotmail.com>
2023-03-28 01:10:58 -0500

    added some css have api

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://dark-sky.p.rapidapi.com/%7Blatitude%7D,%7Blongitude%7D?lang=en&units=auto",
-	"method": "GET",
-	"headers": {
-		"X-RapidAPI-Key": "c2360459b1msh4b28052fa0200ebp137232jsnd765fb5d7108",
-		"X-RapidAPI-Host": "dark-sky.p.rapidapi.com"
-	}
-};
+// // 
+// var weather = localStorage.getItem ("Location").trim();
+// clearTimeout.split(" ").join("20");
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={0ea49ec13420e68ee45ac131d00b02e9}
